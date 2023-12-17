87a686bf0fa6448a2a22d1d8efa4b15680792849
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-12-17 04:29:01 -0300

    FEAT(card-busca): Criando componente CardBuscaComponent

---
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-busca',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-busca.component.html',
+  styleUrl: './card-busca.component.scss'
+})
+export class CardBuscaComponent {
+
+}
