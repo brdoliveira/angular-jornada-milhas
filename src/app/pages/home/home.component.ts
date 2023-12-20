3dbc8f918a8cf25fdaee01a0373ca44e5392add6
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-12-19 23:39:10 -0300

    FEAT(home): Conectando com o backend

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { OnInit } from '@angular/core';
+import { Promocao } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  promocoes!: Promocao[];
 
+  constructor(private servicoPromocao: PromocaoService) {}
+
+  ngOnInit(): void {
+    this.servicoPromocao.listar().subscribe(res => {
+      this.promocoes = res;
+    });
+  }
 }
