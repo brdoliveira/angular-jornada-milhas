175f957da047032657d202e2272f2190c012e643
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-12-24 23:54:44 -0300

    FEAT(home): Removendo standalone e imports

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PromocaoService } from 'src/app/core/services/promocao.service';
 import { OnInit } from '@angular/core';
 import { Promocao } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   promocoes!: Promocao[];
