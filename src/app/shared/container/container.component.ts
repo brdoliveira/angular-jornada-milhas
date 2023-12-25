17f5fd0c6917dadc094d01a4db41fafa5d7fe242
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-12-24 23:54:57 -0300

    FEAT(container): Removendo standalone e imports

--- src/app/shared/container/container.component.ts
+++ src/app/shared/container/container.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-container',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './container.component.html',
-  styleUrl: './container.component.scss'
+  styleUrls: ['./container.component.scss']
 })
 export class ContainerComponent {
 
