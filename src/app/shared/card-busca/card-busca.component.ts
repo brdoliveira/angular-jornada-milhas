3999d3321b1bb4fd459483f2539582e53fcdc504
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-12-24 23:59:33 -0300

    FEAT(card-busca): Removendo standalone e imports

--- src/app/shared/card-busca/card-busca.component.ts
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Promocao } from 'src/app/core/types/type';
 import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-busca',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './card-busca.component.html',
-  styleUrl: './card-busca.component.scss'
+  styleUrls: ['./card-busca.component.scss']
 })
 export class CardBuscaComponent {
   @Input() promocao!: Promocao;
