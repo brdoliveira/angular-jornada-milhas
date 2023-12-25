f34928f125f606847375fdeb4290d8af07aa61fe
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-12-24 23:54:24 -0300

    FEAT(modal): Removendo standalone e imports

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-modal',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.scss'
+  styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
 
