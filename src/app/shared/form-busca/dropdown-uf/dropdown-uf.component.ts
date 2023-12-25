6389f7a10abf485c88f821f4acf5b39a38f6aebc
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-12-25 00:21:33 -0300

    FEAT(dropdown-uf): Criando componente DropdownUfComponent

---
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -0,0 +1,44 @@
+import { UnidadeFederativa } from 'src/app/core/types/type';
+import { UnidadeFederativaService } from './../../../core/services/unidade-federativa.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, map, startWith } from 'rxjs';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-dropdown-uf',
+  templateUrl: './dropdown-uf.component.html',
+  styleUrls: ['./dropdown-uf.component.scss']
+})
+export class DropdownUfComponent implements OnInit{
+  @Input() label: string = "";
+  @Input() iconePrefixo: string = "";
+  @Input() control!: FormControl;
+
+
+  filteredOptions$?: Observable<UnidadeFederativa[]>;
+  unidadesFederativas: UnidadeFederativa[] = [];
+
+  constructor(
+    private unidadeFederativaService: UnidadeFederativaService
+  ){}
+
+  ngOnInit(): void {
+    this.unidadeFederativaService.listar()
+      .subscribe(dados => {
+        this.unidadesFederativas = dados
+        console.log(this.unidadesFederativas)
+      })
+    this.filteredOptions$ = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this.filtrarUfs(value))
+    )
+  }
+
+  filtrarUfs(value: string): UnidadeFederativa[] {
+    const valorFiltrado = value?.toLowerCase();
+    const result = this.unidadesFederativas.filter(
+      estado => estado.nome.toLowerCase().includes(valorFiltrado)
+    )
+    return result
+  }
+}
