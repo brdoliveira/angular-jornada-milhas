22659d7e974327068453c68a7f8743f1fbd7a773
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-12-25 00:22:41 -0300

    FEAT(form-busca): Removendo standalone e imports

--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from '../modal/modal.component';
 import { FormBuscaService } from 'src/app/core/services/form-busca.service';
 
 @Component({
   selector: 'app-form-busca',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './form-busca.component.html',
-  styleUrl: './form-busca.component.scss'
+  styleUrls: ['./form-busca.component.scss']
 })
 export class FormBuscaComponent {
   constructor(
