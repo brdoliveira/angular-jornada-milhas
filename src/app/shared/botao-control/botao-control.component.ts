cb012e0a8c6a38b2808b01c2f70537816f58a634
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-12-26 23:47:02 -0300

    FEAT(botao-control): Removendo standalone e imports

--- src/app/shared/botao-control/botao-control.component.ts
+++ src/app/shared/botao-control/botao-control.component.ts
@@ -1,12 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-botao-controle',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './botao-control.component.html',
-  styleUrl: './botao-control.component.scss'
+  styleUrls: ['./botao-control.component.scss']
 })
 export class BotaoControlComponent {
   @Input() operacao: 'incrementar' | 'decrementar' = "incrementar";
