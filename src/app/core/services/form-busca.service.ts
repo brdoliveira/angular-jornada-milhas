ae72f110dd2a8647ad97e19c4aaf34c9e8539093
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-01-03 23:44:04 -0300

    FEAT(form-busca)? Adicionando método trocarOrigemDestino()

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -67,4 +67,13 @@ export class FormBuscaService {
   
     return descricao
   }
+
+  trocarOrigemDestino(): void {
+    const origem = this.formBusca.get('origem')?.value;
+    const destino = this.formBusca.get('destino')?.value;
+  
+    this.formBusca.patchValue({
+      origem: destino,
+      destino: origem
+    });
 }
