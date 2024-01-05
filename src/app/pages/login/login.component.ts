7ff7c30bfb3e968abcfe01145fcccd63e862be54
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-01-05 00:47:05 -0300

    FEAT(login): Formatando codigo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,21 +4,21 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-    
-    constructor(private formBuilder: FormBuilder) {}
-    
-    ngOnInit(): void {
-      this.loginForm = this.formBuilder.group({
-          email: [null],
-          senha: [null]
-      })
-    }
 
-    login() {
-      console.log('Login realizado com sucesso', this.loginForm.value)
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [null],
+      senha: [null],
+    });
+  }
+
+  login() {
+    console.log('Login realizado com sucesso', this.loginForm.value);
   }
 }
