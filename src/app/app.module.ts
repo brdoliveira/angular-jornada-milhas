d1d549f9eb26a55b679b3449c3cdfe525bc45034
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-05-03 23:56:21 -0300

    FEAT(app): Importando CadastroComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { FormBaseComponent } from './shared/form-base/form-base.component';
 import { MatRadioModule  } from '@angular/material/radio';
 import { MatDividerModule   } from '@angular/material/divider';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 
 
@@ -56,7 +57,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     DepoimentosComponent,
     SeletorPassageiroComponent,
     LoginComponent,
-    FormBaseComponent
+    FormBaseComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
