d36a6517494b314e15340851e22a50fea2b32a7a
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-05-06 23:59:10 -0300

    REFACT(autenticacao): Refatorando método autenticar()

--- src/app/core/services/autenticacao.service.ts
+++ src/app/core/services/autenticacao.service.ts
@@ -1,18 +1,39 @@
 import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { UserService } from './user.service';
+
+interface AuthResponse {
+  access_token: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutenticacaoService {
-
   private apiUrl: string = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private userService: UserService
+  ) {}
 
-  autenticar(email: string, senha: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/auth/login`, { email, senha })
-}
+  autenticar(
+    email: string,
+    senha: string
+  ): Observable<HttpResponse<AuthResponse>> {
+    return this.http
+      .post<AuthResponse>(
+        `${this.apiUrl}/auth/login`,
+        { email, senha },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((response) => {
+          const authToken = response.body?.access_token || '';
+          this.userService.salvarToken(authToken);
+        })
+      );
+  }
 }
