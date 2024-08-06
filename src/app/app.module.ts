a86dc2aa0fa6725abf089586b2ccf3f913041bf8
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-05 23:49:03 -0300

    FEAT(app.module): Importando modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,42 +4,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { BannerComponent } from './shared/banner/banner.component';
 import { CardComponent } from './shared/card/card.component';
-import { FormBuscaComponent } from './shared/form-busca/form-busca.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { ContainerComponent } from './shared/container/container.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
 import { MatCardModule } from '@angular/material/card';
+import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
+import { FormBuscaComponent } from './shared/form-busca/form-busca.component';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { DropdownUfComponent } from './shared/dropdown-uf/dropdown-uf.component';
-import { ContainerComponent } from './shared/container/container.component';
-import { HomeComponent } from './pages/home/home.component';
-import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
-import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
+import { MatNativeDateModule } from '@angular/material/core';
 import { ModalComponent } from './shared/modal/modal.component';
-import { DepoimentosComponent } from './pages/home/depoimentos/depoimentos.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+import { BotaoControleComponent } from './shared/botao-controle/botao-controle.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PromocoesComponent } from './pages/home/promocoes/promocoes.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DropdownUfComponent } from './shared/dropdown-uf/dropdown-uf.component';
 import { SeletorPassageiroComponent } from './shared/seletor-passageiro/seletor-passageiro.component';
+import { DepoimentosComponent } from './pages/home/depoimentos/depoimentos.component';
 import { LoginComponent } from './pages/login/login.component';
 import { FormBaseComponent } from './shared/form-base/form-base.component';
-import { MatRadioModule  } from '@angular/material/radio';
-import { MatDividerModule   } from '@angular/material/divider';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
-
-
 
 @NgModule({
   declarations: [
@@ -54,9 +55,11 @@ import { PerfilComponent } from './pages/perfil/perfil.component';
     CardDepoimentoComponent,
     FormBuscaComponent,
     ModalComponent,
+    BotaoControleComponent,
+    PromocoesComponent,
     DropdownUfComponent,
-    DepoimentosComponent,
     SeletorPassageiroComponent,
+    DepoimentosComponent,
     LoginComponent,
     FormBaseComponent,
     CadastroComponent,
@@ -82,8 +85,8 @@ import { PerfilComponent } from './pages/perfil/perfil.component';
     MatAutocompleteModule,
     MatRadioModule,
     MatDividerModule,
-    MatCheckboxModule 
-],
+    MatCheckboxModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
