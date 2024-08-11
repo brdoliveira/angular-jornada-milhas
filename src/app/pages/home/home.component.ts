62996aa843b8a6f05ac4e283ec71252f14f25b96
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-11 00:34:35 -0300

    REFACT(home): Melhorando funcionamento do componente home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { PromocaoService } from 'src/app/core/services/promocao.service';
 import { OnInit } from '@angular/core';
-import { Promocao } from 'src/app/core/types/type';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +9,21 @@ import { Promocao } from 'src/app/core/types/type';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  promocoes!: Promocao[];
-
-  constructor(private servicoPromocao: PromocaoService) {}
+  constructor(
+    private servicoPromocao: PromocaoService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.servicoPromocao.listar().subscribe(res => {
-      this.promocoes = res;
-    });
+    this.servicoPromocao.listar()
+      .subscribe(
+        resposta => {
+          console.log(resposta)
+        }
+      )
+  }
+
+  navegarParaBusca(ev: any) {
+    this.router.navigate(['busca']);
   }
 }
