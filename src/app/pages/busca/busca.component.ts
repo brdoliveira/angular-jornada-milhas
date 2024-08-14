c0fe3d01664a5c0a2d8cb41f4775b2f3a332ac1b
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-13 23:56:38 -0300

    FEAT(busca): Criando component BuscaComponent

--- src/app/pages/busca/busca.component.ts
+++ src/app/pages/busca/busca.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PassagensService } from 'src/app/core/services/passagens.service';
 
 @Component({
   selector: 'app-busca',
   templateUrl: './busca.component.html',
   styleUrls: ['./busca.component.scss']
 })
-export class BuscaComponent {
+export class BuscaComponent implements OnInit {
+  passagens: Passagens[] = []
 
+  constructor(private passagensService: PassagensService) { }
+  ngOnInit(): void {
+    const filtroPadrao = {
+      dataIda: new Date().toISOString(),
+      pagina: 1,
+      porPagina: 25,
+      somenteIda: false,
+      passageirosAdultos: 1,
+      tipo: 'Executiva'
+    }
+    this.passagensService.getPassagens(filtroPadrao)
+      .subscribe(res => {
+        this.passagens = res.resultado;
+        console.log(this.passagens);
+      })
+  }
 }
