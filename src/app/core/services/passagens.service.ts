ab91f7777817ee45780dd9a43e4bc2af40d944c2
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-13 23:57:08 -0300

    FEAT(passagens.service): Criando classe PassagensService

---
+++ src/app/core/services/passagens.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PassagensService {
+  apiUrl: string = environment.apiUrl;
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+  getPassagens(search: any): Observable<Passagens[]> {
+    const params = search;
+    return this.httpClient.get<Passagens[]>(this.apiUrl + '/passagem/search', { params })
+  }
+}
