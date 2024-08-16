5247084733e89d92db9017630da5efa5d35176dd
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-15 23:22:32 -0300

    FEAT(passagens): Tipando getPassagens()

--- src/app/core/services/passagens.service.ts
+++ src/app/core/services/passagens.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Resultado } from '../types/type';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ export class PassagensService {
   constructor(
     private httpClient: HttpClient
   ) { }
-  getPassagens(search: any): Observable<Passagens[]> {
+  getPassagens(search: any): Observable<Resultado> {
     const params = search;
-    return this.httpClient.get<Passagens[]>(this.apiUrl + '/passagem/search', { params })
+    return this.httpClient.get<Resultado>(this.apiUrl + '/passagem/search', { params })
   }
 }
