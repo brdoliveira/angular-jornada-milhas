47aa4831947281161ca57fc87f423d262f997bed
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-17 09:14:31 -0300

    FEAT(busca): Criando método busca

--- src/app/pages/busca/busca.component.ts
+++ src/app/pages/busca/busca.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuscaService } from 'src/app/core/services/form-busca.service';
 import { PassagensService } from 'src/app/core/services/passagens.service';
-import { Passagem } from 'src/app/core/types/type';
+import { DadosBusca, Passagem } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-busca',
@@ -10,7 +11,10 @@ import { Passagem } from 'src/app/core/types/type';
 export class BuscaComponent implements OnInit {
   passagens: Passagem[] = []
 
-  constructor(private passagensService: PassagensService) { }
+  constructor(
+    private passagensService: PassagensService,
+    private formBuscaService: FormBuscaService
+  ) { }
   ngOnInit(): void {
     const filtroPadrao = {
       dataIda: new Date().toISOString(),
@@ -20,10 +24,21 @@ export class BuscaComponent implements OnInit {
       passageirosAdultos: 1,
       tipo: 'Executiva'
     }
-    this.passagensService.getPassagens(filtroPadrao)
-      .subscribe(res => {
-        this.passagens = res.resultado;
-        console.log(this.passagens);
+
+    const busca = this.formBuscaService.formEstaValido ? this.formBuscaService.obterDadosBusca() : filtroPadrao
+    this.passagensService.getPassagens(busca).subscribe(
+      res => {
+        console.log(res)
+        this.passagens = res.resultado
+      }
+    )
+  }
+
+  busca(ev: DadosBusca) {
+    this.passagensService.getPassagens(ev).subscribe(
+      res => {
+        console.log(res)
+        this.passagens = res.resultado
       })
   }
 }
