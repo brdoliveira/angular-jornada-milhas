1014a810a78ff3b26de6f6690f0512d979772db9
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-17 09:15:12 -0300

    REFACT(form-busca): Melhorando método de buscar

--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -10,14 +10,15 @@ export class FormBuscaComponent {
   @Output() realizarBusca = new EventEmitter();
 
   constructor(
-    public formBuscaService : FormBuscaService) {}
+    public formBuscaService : FormBuscaService
+  ) {}
 
-  buscar () {
-    if (this.formBuscaService.formEstaValido) {
-      const formBuscavalue = this.formBuscaService.formBusca.value;
-      this.realizarBusca.emit(formBuscavalue);
-    } else {
-      alert('O formulário precisa ser preenchido')
-    }
+  buscar() {
+      if (this.formBuscaService.formEstaValido) {
+        const formBuscavalue = this.formBuscaService.obterDadosBusca();
+        this.realizarBusca.emit(formBuscavalue);
+      } else {
+        alert('O formulário precisa ser preenchido')
+      }
   }
 }
