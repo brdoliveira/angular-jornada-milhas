59d3053587211c83147d2f9dd080ffaf15f06f2a
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-27 23:51:25 -0300

    FEAT(app): Removendo imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './pages/home/home.component';
-
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { PromocoesComponent } from './pages/home/promocoes/promocoes.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { DepoimentosComponent } from './pages/home/depoimentos/depoimentos.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
@@ -17,13 +13,11 @@ import { AutenticacaoInterceptor } from './core/interceptors/autenticacao.interc
 import { BuscaComponent } from './pages/busca/busca.component';
 import { SharedModule } from './shared/shared.module';
 import { MaterialModule } from './core/material/material.module';
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    PromocoesComponent,
-    DepoimentosComponent,
     LoginComponent,
     CadastroComponent,
     PerfilComponent,
@@ -34,6 +28,7 @@ import { MaterialModule } from './core/material/material.module';
     AppRoutingModule,
     SharedModule,
     MaterialModule,
+    HomeModule,
     BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule
