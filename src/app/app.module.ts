842c5f68712c0304fff9046855abb054497c7f53
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-28 00:39:22 -0300

    FEAT(app): Ajustando app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,16 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
-import { CadastroComponent } from './pages/cadastro/cadastro.component';
-import { PerfilComponent } from './pages/perfil/perfil.component';
-import { AutenticacaoInterceptor } from './core/interceptors/autenticacao.interceptor';
-import { BuscaComponent } from './pages/busca/busca.component';
+import { AutenticacaoInterceptor } from './autenticacao/autenticacao.interceptor';
+import { BuscaComponent } from './busca/busca.component';
 import { SharedModule } from './shared/shared.module';
 import { MaterialModule } from './core/material/material.module';
-import { HomeModule } from './pages/home/home.module';
+import { AutenticacaoModule } from './autenticacao/autenticacao.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    CadastroComponent,
-    PerfilComponent,
     BuscaComponent
   ],
   imports: [
@@ -29,6 +24,7 @@ import { HomeModule } from './pages/home/home.module';
     SharedModule,
     MaterialModule,
     HomeModule,
+    AutenticacaoModule,
     BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule
