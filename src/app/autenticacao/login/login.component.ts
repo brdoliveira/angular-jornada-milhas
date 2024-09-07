951b682ac018673a2798a9cbc28946880701e095
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-09-07 01:06:32 -0300

    FEAT(login): Alterando import

--- src/app/autenticacao/login/login.component.ts
+++ src/app/autenticacao/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
 import { Router } from '@angular/router';
+import { AutenticacaoService } from '../services/autenticacao.service';
 
 @Component({
   selector: 'app-login',
