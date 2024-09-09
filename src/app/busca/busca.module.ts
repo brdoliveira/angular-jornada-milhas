9d2b791a84fc3a8aa95326191e59702fb60c7c5c
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-09-08 23:55:44 -0300

    FEAT(busca): Importando modulo de rota

--- src/app/busca/busca.module.ts
+++ src/app/busca/busca.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '../core/material/material.module';
+import { BuscaRoutingModule } from './busca-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    SharedModule,
+    ReactiveFormsModule,
+    BuscaRoutingModule
   ]
 })
 export class BuscaModule { }
